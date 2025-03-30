ddb2e8b4df8ad0d9999bd202c2c94cd1767de960
tirmo05 <tirsomorato@gmail.com>
2025-03-30 15:49:16 +0200

    :)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import SignIn from './SignIn'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SignIn />
   </StrictMode>,
 )
